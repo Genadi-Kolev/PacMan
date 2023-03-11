e528c377f2545315d809a33a651f51328af9b4a4
Genadi Kolev <genadimkolev@gmail.com>
2023-03-12 00:49:43 +0200

    Movement controls
    game loop consumes current input of sprite

--- source/engine.js
+++ source/engine.js
@@ -1,36 +1,25 @@
-let canvas;
-let context;
+import Input from './input.js';
 
 window.onload = init;
 
+const input = new Input()
+
+class GameEngine {
+
+    init() {
+        input.init();
+        setInterval(this.gameLoop, 16)
+    };
+
+    gameLoop(_timeStamp) {
+        input.consumeInput()
+
+    }
+};
+
+const engine = new GameEngine()
+
 function init() {
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
+    engine.init();
+};
 
-    document.addEventListener('keypress', (event) => {
-        let name = event.key;
-        let code = event.code;
-
-        handleInput(code, name)
-
-    }, false);
-
-    requestAnimationFrame(gameLoop);
-}
-
-function gameLoop(_timeStamp) {
-    // draw()
-
-    requestAnimationFrame(gameLoop);
-}
-
-function draw() {
-    let randomColour = Math.random() > 0.5? '#ff8080': '#0099b0';
-    context.fillStyle = randomColour
-    context.fillRect(100, 50, 200, 175);
-}
-
-function handleInput(code, name) {
-        document.getElementById('key_code').innerHTML = code
-        document.getElementById('key_name').innerText = name
-}
--- source/input.js
+++ source/input.js
@@ -1,69 +1,89 @@
-// Declare and assign variables.
-let object1Size = {
-    width: 20,
-    height: 20
+export default class Input {
+    // Declare and assign variables.
+    moveRate = 2.2;
+
+    object1Size = {
+        width: 20,
+        height: 20
+    };
+    position = {
+        x: 10,
+        y: 10
+    };
+    input = {
+        x: 0,
+        y: 0
+    };
+
+    object1 = document.getElementById("object1");
+
+
+    init() {
+        document.addEventListener('keydown', (event) => {
+            if (event.defaultPrevented) {
+                return;
+            }
+            switch (event.code) {
+                case 'KeyS':
+                    this.input.x = 0;
+                    this.input.y = -1;
+                    break;
+                case 'KeyW':
+                    this.input.x = 0;
+                    this.input.y = 1;
+                    break;
+                case 'KeyA':
+                    this.input.x = -1;
+                    this.input.y = 0;
+                    break;
+                case 'KeyD':
+                    this.input.x = 1;
+                    this.input.y = 0;
+                    break;
+                case 'Escape':
+                    this.input.x = 0;
+                    this.input.y = 0;
+                    break;
+            }
+            event.preventDefault();
+        }, false);
+    };
+
+    consumeInput() {
+        this.updateXPosition(this.moveRate * this.input.x)
+        this.updateYPosition(this.moveRate * this.input.y)
+
+        this.refreshPosition()
+    };
+
+    updateYPosition(distance) {
+        this.position.y -= distance;
+        // Update y-axis position at the edge.
+        if (this.position.y < 0) {
+            this.position.y = 499;
+        } 
+        else if (this.position.y > 499) {
+            this.position.y = 0;
+        }
+    };
+
+    // Update x-axis position.
+    updateXPosition(distance) {
+        this.position.x += distance;
+        // Update x-axis position at the edge.
+        if (this.position.x < 0) {
+            this.position.x = 499;
+        } 
+        else if (this.position.x > 499) {
+            this.position.x = 0;
+        }
+    };
+
+    refreshPosition() {
+        let x = this.position.x - (this.object1Size.width/2);
+        let y = this.position.y - (this.object1Size.height/2);
+        let transform = "translate(" + x + " " + y + ")";
+
+        object1.setAttribute("transform", transform);
+    };
 };
-let position = {
-    x: 10,
-    y: 10
-};
-let moveRate = 10;
-let object1 = document.getElementById("object1");
-// Update y-axis position.
-
-function updateYPosition(distance) {
-    position.y -= distance;
-    // Update y-axis position at the edge.
-    if (position.y < 0) {
-        position.y = 499;
-    } else if (position.y > 499) {
-        position.y = 0;
-    }
-}
-
-// Update x-axis position.
-function updateXPosition(distance) {
-    position.x += distance;
-    // Update x-axis position at the edge.
-    if (position.x < 0) {
-        position.x = 499;
-    } else if (position.x > 499) {
-        position.x = 0;
-    }
-}
-
-function refreshPosition() {
-    let x = position.x - (object1Size.width/2);
-    let y = position.y - (object1Size.height/2);
-    let transform = "translate(" + x + " " + y + ")";
-
-    object1.setAttribute("transform", transform);
-}
-
-window.addEventListener("keydown", function(event) {
-    if (event.defaultPrevented) {
-        return;
-    }
-    switch (event.code) {
-        case "ArrowDown":
-            // Handle "down"
-            updateYPosition(-moveRate);
-            break;
-        case "ArrowUp":
-            // Handle "up"
-            updateYPosition(moveRate);
-            break;
-        case "ArrowLeft":
-            // Handle "left"
-            updateXPosition(-moveRate);
-            break;
-        case "ArrowRight":
-            // Handle "right"
-            updateXPosition(moveRate);
-            break;
-    }
-
-    refreshPosition();
-    event.preventDefault();
-
-}, true);
